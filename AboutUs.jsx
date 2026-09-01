ca4c04c778a398d36c0bc548de58ded80cc9c3b1
Arnav Sapkal <arnavsapkal1@gmail.com>
2026-09-02 00:58:36 +0530

    Create AboutUs.jsx

---
+++ AboutUs.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+function AboutUs() {
+  return (
+    <div style={{ padding: "40px" }}>
+      <h1>About Paradise Nursery</h1>
+
+      <p>
+        Paradise Nursery is your trusted destination for beautiful indoor
+        plants that bring freshness and happiness into every home.
+      </p>
+
+      <p>
+        We carefully select healthy houseplants and provide affordable prices
+        so everyone can enjoy nature indoors.
+      </p>
+
+      <p>
+        Our mission is to make homes greener while encouraging a healthier and
+        more peaceful lifestyle.
+      </p>
+    </div>
+  );
+}
+
+export default AboutUs;
